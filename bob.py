729d91dc6876f196684b26431233d5d4891825d6
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2025-10-25 15:49:01 -0700

    Finished run 4

--- bob.py
+++ bob.py
@@ -25,140 +25,52 @@ class Bob:
         )
         self.drivebase.use_gyro(True)
 
+        self.factor = 1
+
+    def set_speed_factor(self, factor):
+        self.factor = factor
+
     def foreward(self, distance, speed):
         print("move fwd " + str(distance))
         default_speed = self.drivebase.settings()[0]
-        self.drivebase.settings(straight_speed=speed)
+        self.drivebase.settings(straight_speed=self.factor*speed)
         self.drivebase.straight(distance, then=Stop.BRAKE, wait=True)
         self.drivebase.settings(straight_speed=default_speed)
 
     def reverse(self, distance, speed):
         print("move backward " + str(distance))
         default_speed = self.drivebase.settings()[0]
-        self.drivebase.settings(straight_speed=speed)
+        self.drivebase.settings(straight_speed=self.factor*speed)
         self.drivebase.straight(-distance, then=Stop.BRAKE, wait=True)
         self.drivebase.settings(straight_speed=default_speed)
 
     def turn(self, degree, speed=200):
         print("turning " + str(degree))
         default_settings = self.drivebase.settings()
-        self.drivebase.settings(default_settings[0], default_settings[1], speed, default_settings[3])
+        self.drivebase.settings(default_settings[0], default_settings[1], self.factor*speed, default_settings[3])
         self.drivebase.turn(degree, then=Stop.HOLD, wait=True)
         self.drivebase.settings(default_settings[0], default_settings[1], default_settings[2], default_settings[3])
     
-    def turn_front_motor(self, degree, speed):
+    def turn_front_motor(self, degree, speed, then=Stop.HOLD):
         print("turning front motor " +str(degree))
-        self.attachment_motor.run_angle(speed=speed, rotation_angle=degree, then=Stop.BRAKE, wait=True)
+        self.attachment_motor.run_angle(speed=self.factor*speed, rotation_angle=degree, then=then, wait=True)
 
     def turn_back_motor(self, degree, speed):
         print("turning front motor " +str(degree))
-        self.back_motor.run_angle(speed=speed, rotation_angle=degree, then=Stop.BRAKE, wait=True)
+        self.back_motor.run_angle(speed=self.factor*speed, rotation_angle=degree, then=Stop.BRAKE, wait=True)
 
-    def drive(self, speed, turn_rate):
-        """Drive at specified speed and turn rate"""
-        print(f"driving at speed {speed}, turn rate {turn_rate}")
-        self.drivebase.drive(speed, turn_rate)
+    # def drive(self, speed, turn_rate):
+    #     """Drive at specified speed and turn rate"""
+    #     print(f"driving at speed {speed}, turn rate {turn_rate}")
+    #     self.drivebase.drive(self.factor*speed, turn_rate)
     
-    def stop(self):
-        """Stop the drivebase"""
-        print("stopping")
-        self.drivebase.stop()
+    # def stop(self):
+    #     """Stop the drivebase"""
+    #     print("stopping")
+    #     self.drivebase.stop()
     
-    def attachment_run_angle(self, speed, angle):
-        """Run attachment motor at specified speed for specified angle"""
-        print(f"attachment motor running at speed {speed} for angle {angle}")
-        self.attachment_motor.run_angle(speed, angle)
-    
-    # def execute(self, filename):
-    #     """
-    #     Play a noise and then execute a specific robot program.
-        
-        def drive(self, speed, turn_rate):
-            """Drive at specified speed and turn rate"""
-            print(f"driving at speed {speed}, turn rate {turn_rate}")
-            self.drivebase.drive(speed, turn_rate)
-        
-        def stop(self):
-            """Stop the drivebase"""
-            print("stopping")
-            self.drivebase.stop()
-        
-        def attachment_run_angle(self, speed, angle):
-            """Run attachment motor at specified speed for specified angle"""
-            print(f"attachment motor running at speed {speed} for angle {angle}")
-            self.attachment_motor.run_angle(speed, angle)
-        
-        # def execute(self, filename):
-        #     """
-        #     Play a noise and then execute a specific robot program.
-            
-        #     Args:
-        #         filename: Name of the robot program to execute
-        #     """
-        #     print(f"Starting execution of {filename}...")
-            
-        #     # Play a startup sound
-        #     print("Playing startup sound...")
-        #     self.hub.speaker.beep(523, 200)  # C5
-        #     self.hub.speaker.beep(659, 200)  # E5
-        #     self.hub.speaker.beep(784, 200)  # G5
-        #     self.hub.speaker.beep(1047, 200) # C6
-            
-        #     try:
-        #         # Execute specific robot programs
-        #         if filename == "mission2akewl":
-        #             self.run_mission2akewl()
-        #         else:
-        #             print(f"Unknown program: {filename}")
-        #             return
-                    
-        #     except Exception as e:
-        #         print(f"Error executing {filename}: {e}")
-            
-        #     # Play completion sound
-        #     print("Playing completion sound...")
-        #     self.hub.speaker.beep(784, 200)  # G5
-        #     self.hub.speaker.beep(659, 200)  # E5
-        #     self.hub.speaker.beep(523, 200)  # C5
-        
-        # def run_mission2akewl(self):
-        #     """Run the mission2akewl robot program"""
-        #     print("Running mission2akewl program...")
-            
-        #     # Move attachment motor up
-        #     self.attachment_run_angle(100, 250)
-            
-        #     # Drive forward
-        #     self.drive(300, 0)
-        #     wait(2600)
-        #     self.stop()
-            
-        #     # Turn left
-        #     self.turn(-20)
-            
-        #     # Move attachment motor down slightly
-        #     self.attachment_run_angle(100, -75)
-            
-        #     # Drive forward a bit
-        #     self.drive(150, 0)
-        #     wait(500)
-        #     self.stop()
-            
-        #     # Move attachment motor down more
-        #     self.attachment_run_angle(100, -150)
-            
-        #     # Drive backward
-        #     self.drive(-200, 0)
-        #     wait(500)
-        #     self.stop()
-            
-        #     # Turn right
-        #     self.turn(30)
-            
-        #     # Drive backward
-        #     self.drive(-300, 0)
-        #     wait(2600)
-        #     self.stop()
-            
-        #     print("Mission2akewl program completed!")
+    # def attachment_run_angle(self, speed, angle):
+    #     """Run attachment motor at specified speed for specified angle"""
+    #     print(f"attachment motor running at speed {speed} for angle {angle}")
+    #     self.attachment_motor.run_angle(self.factor*speed, angle)
         
--- run4.py
+++ run4.py
@@ -9,38 +9,39 @@ from pybricks.tools import wait
 from bob import Bob
 
 def execute(bob: Bob):
-    bob.foreward(750,500)
-    bob.turn(-34,150)
+    bob.turn_front_motor(395,500)
+    bob.foreward(740,500)
+    bob.turn(-34,250)
     wait(200)
-    bob.turn(95,250)
-    bob.turn(-55,250)
+    bob.turn(130,100)
+
+    bob.foreward(120, 100)
+    bob.turn_front_motor(60,100, then=Stop.BRAKE)
+    bob.turn_front_motor(10,20, then=Stop.HOLD)
+    bob.foreward(180,300)
+    
+    # Just knocked off the weighted thing
+
+    bob.foreward(-155,300)
+    bob.turn_front_motor(-150,100)
+    bob.foreward(-155,300)
+    
+    # Moved back    
+    
+    bob.turn(-90,250)
     bob.foreward(-30,250)
     bob.turn(-20,100)
     bob.foreward(12,250)
-    bob.turn(-30,100)
-    bob.foreward(-28,150)
-    #bob.foreward(15,100)
+    bob.turn(-40,100)
+    bob.foreward(-30,150)
+
+    # Moved into position
     for crash in range(5):    
         bob.turn_back_motor(380,4000)
         bob.turn_back_motor(-380,4000)
+
+    # Got all the pills out    
     bob.turn(45,250)
     bob.foreward(-700,1000)
-    # bob.turn(degree=-29, speed=60)
-    # bob.turn(degree=38, speed=60)
-    # bob.foreward(distance=-45, speed=50)
-    # bob.turn(degree=30, speed=60)
-    # bob.foreward(distance=20, speed=50)
-    # bob.foreward(10,50)
-    # bob.turn_front_motor(degree=300, speed=60)
-    # bob.foreward(distance=13, speed=100)
-    # bob.turn_front_motor(degree=-300, speed=100)
-    # bob.foreward(distance=-25, speed=100)
-    # bob.turn(degree=-50)
-    # bob.foreward(distance=-25, speed=100)
-    # bob.foreward(-50,100)
-    # bob.turn(-54)
-    # bob.foreward(25,100)
-    # for crash in range(5):    
-    #     bob.turn_back_motor(degree=350, speed=2000)
-    #     bob.turn_back_motor(degree=-350, speed=2000)
-        
+    
+    # Back home
