767113bb0ebbf05b58e9f44e6137f6917d140be9
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2025-10-25 21:48:05 -0700

    run 2 fixes

--- run2.py
+++ run2.py
@@ -10,12 +10,10 @@ from bob import Bob
 
 def execute(bob: Bob):
 
-    #bob.foreward(745,1000)
     bob.foreward(745,500)
 
     bob.turn(-25,60)
 
-    #bob.foreward(25,100)
     bob.foreward(20,100)
 
     bob.turn_front_motor(-300, 300)
@@ -30,66 +28,24 @@ def execute(bob: Bob):
 
     bob.reverse(125,1000)
 
-    bob.turn(78,100)
+    bob.turn(72,100)
 
     bob.reverse(100,1000)
 
     bob.turn_back_motor(450,500)
 
-    bob.reverse(120,300)
+    bob.reverse(110,300)
     
-    bob.turn_back_motor(150,-100)
+    bob.turn_back_motor(-150,200)
 
     bob.turn(-30)
+    
+    wait(200)
 
     bob.turn(40)
 
     bob.foreward(250,1000)
 
-    bob.turn(80)
+    bob.turn(60)
 
-    bob.reverse(700,1000)
-
-    # bob.reverse(700,400)
-
-    
-
-# Initialize hub
-# hub = PrimeHub()
-
-# # Drive motors
-# left_motor = Motor(Port.B, positive_direction=Direction.COUNTERCLOCKWISE)
-# right_motor = Motor(Port.F)
-
-# # Attachment motor (with small fixture + gears)
-# attachment_motor = Motor(Port.D)
-
-# # Set up drivebase
-# drivebase = DriveBase(left_motor, right_motor, wheel_diameter=89, axle_track=152)
-
-# attachment_motor.run_angle(100,250)
-
-# drivebase.drive(300,0)
-# wait(2600)
-# drivebase.stop()
-
-# drivebase.turn(-20)
-
-# attachment_motor.run_angle(100,-75)
-
-# drivebase.drive(150,0)
-# wait(500)
-# drivebase.stop()
-
-# attachment_motor.run_angle(100,-150)
-
-# drivebase.drive(-200,0)
-# wait(500)
-# drivebase.stop()
-
-# drivebase.turn(30)
-
-# drivebase.drive(-300,0)
-# wait(2600)
-# drivebase.stop()
-
+    bob.reverse(800,1000)
