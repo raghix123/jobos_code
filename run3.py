22448349bf74f3ab74823427cc17624290d5776d
Rishaan Krishnan <rishaan.krishnan@gmail.com>
2025-11-02 16:11:03 -0800

    run3 changes

--- run3.py
+++ run3.py
@@ -4,17 +4,17 @@ from pybricks.tools import wait
 
 def execute(bob: Bob):
 
-    bob.foreward(distance=700, speed=400)
+    # bob.foreward(distance=700, speed=400)
 
-    bob.turn(63,60)
+    # bob.turn(60,60)
 
-    bob.foreward(distance=230, speed=200)
+    # bob.foreward(distance=175, speed=200)
 
-    bob.turn_front_motor(175,200)
+    bob.turn_front_motor(200,200)
 
     wait(200)
 
-    bob.turn_back_motor(420,300)
+    bob.turn_back_motor(500,300)
 
     bob.reverse(220,400)
 
