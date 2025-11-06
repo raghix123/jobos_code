78dd56b045ece44df25c1aae8c907ac57f641238
Rishaan Krishnan <rishaan.krishnan@gmail.com>
2025-11-05 17:06:26 -0800

    run3 very reliable

--- run3.py
+++ run3.py
@@ -4,29 +4,31 @@ from pybricks.tools import wait
 
 def execute(bob: Bob):
 
-    # bob.foreward(distance=700, speed=400)
+    bob.foreward(distance=700, speed=400)
 
-    # bob.turn(60,60)
+    bob.turn(58,60)
 
-    # bob.foreward(distance=175, speed=200)
+    bob.foreward(distance=185, speed=200)
 
-    bob.turn_front_motor(200,200)
+    bob.turn_front_motor(270,200)
 
     wait(200)
 
-    bob.turn_back_motor(500,300)
+    bob.turn_front_motor(-270,200)
 
-    bob.reverse(220,400)
+    bob.turn_back_motor(350,300)
 
-    bob.turn(10,100)
+    bob.reverse(200,400)
 
-    bob.reverse(50,100)
+    bob.turn(17,100)
 
-    bob.turn_back_motor(-200,300)
+    bob.reverse(120,100)
+
+    bob.turn_back_motor(-200,200)
 
     bob.foreward(100,1000)
 
-    bob.turn(-45,100)
+    bob.turn(-60,100)
 
     bob.reverse(600,1000)
 
