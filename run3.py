ea5983f9663f15e84976149165ce4b01b638a307
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2025-11-08 16:24:20 -0800

    run3

--- run3.py
+++ run3.py
@@ -6,7 +6,7 @@ from pybricks.parameters import Stop
 def execute(bob: Bob):
     bob.foreward(distance=320, speed=400, then=Stop.BRAKE)
     bob.arc(radius=550, angle=50, then=Stop.BRAKE)
-    bob.turn_front_motor_dc(dc=60, time=485)
+    bob.turn_front_motor_dc(dc=60, time=600)
     wait(1000)
     bob.turn_front_motor(degree=-180, speed=200, then=Stop.COAST)
     # Minecart sent
@@ -17,37 +17,5 @@ def execute(bob: Bob):
     # Brush picked up
 
     bob.arc(radius=90, angle=90, then=Stop.BRAKE)
-    bob.foreward_and_front_motor(foreward_distance=300, foreward_speed=500, turn_degree=180, turn_speed=200)
-
-
-    # bob.drivebase.arc(radius=90, angle=90)
-
-
-    # bob.foreward(distance=700, speed=400)
-
-    # bob.turn(58,60)
-
-    # bob.foreward(distance=185, speed=200)
-
-    # bob.turn_front_motor(270,200)
-
-    # wait(200)
-
-    # bob.turn_front_motor(-270,200)
-
-    # bob.turn_back_motor(350,300)
-
-    # bob.reverse(200,400)
-
-    # bob.turn(17,100)
-
-    # bob.reverse(120,100)
-
-    # bob.turn_back_motor(-200,200)
-
-    # bob.foreward(100,1000)
-
-    # bob.turn(-60,100)
-
-    # bob.reverse(600,1000)
-
+    bob.foreward_and_front_motor(foreward_distance=450, foreward_speed=500, turn_degree=180, turn_speed=200)
+    # Back home
