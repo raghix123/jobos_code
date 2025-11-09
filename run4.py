a467b3aa447b916da9aebcc6e8f407cc6c20ec74
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2025-11-09 12:04:32 -0800

    adding a calib step and run4 cleanup

--- run4.py
+++ run4.py
@@ -9,7 +9,7 @@ from bob import Bob
 def execute(bob: Bob):
     bob.back_motor.reset_angle(0)
     bob.attachment_motor.reset_angle(0)
-    bob.foreward_and_front_motor(foreward_distance=660, foreward_speed=500, turn_degree=395, turn_speed=500)
+    bob.foreward_and_front_motor(foreward_distance=640, foreward_speed=500, turn_degree=395, turn_speed=200)
     # Ready
 
     for hit in range(5):
@@ -17,7 +17,7 @@ def execute(bob: Bob):
         bob.back_motor.run_target(speed=400, target_angle=0, then = Stop.BRAKE)
     # Silo done
     
-    bob.foreward(70, 200)
+    bob.foreward(90, 200)
     bob.turn(degree=-24, speed=250, then=Stop.COAST)
     wait(200)
     # Who lived here done
@@ -30,14 +30,14 @@ def execute(bob: Bob):
     bob.foreward(120, 100)
     # # Boulders recovered
 
-    bob.attachment_motor.run_target(speed=500, target_angle=470, then=Stop.HOLD)
-    bob.foreward(210,300)
+    bob.attachment_motor.run_target(speed=200, target_angle=470, then=Stop.HOLD)
+    bob.foreward(distance=210, speed=300)
     # Heavy lifting done
 
     bob.turn_front_motor(degree=-90, speed=100, then=Stop.BRAKE)
     # Lifting the arm
 
-    bob.foreward(distance=-250, speed=300, then=Stop.BRAKE)
+    bob.foreward(distance=-270, speed=300, then=Stop.BRAKE)
     bob.turn(90)
     bob.foreward(740,500)
     # Home
