18b92edd0ddf8d9056e6a4dcfa50b381608b671f
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2025-11-11 22:40:40 -0800

    run2 changes to arcs

--- run2.py
+++ run2.py
@@ -9,43 +9,29 @@ from pybricks.tools import wait
 from bob import Bob
 
 def execute(bob: Bob):
-    bob.turn_front_motor(-40,100)
-    # Ready
+    bob.foreward_and_front_motor(foreward_distance=525, foreward_speed=400, turn_degree=-110, turn_speed=300)
+    bob.foreward(-150, 300)
+    bob.foreward(200, 400)
+    # Soil deposit removed
 
-    bob.foreward(745,500)
-    # Soil deposit #1 done
-
-    bob.turn(-27,60)
-    bob.foreward(55,100)
+    bob.turn_front_motor(80, 100)
+    bob.arc(radius=-460, angle=25, speed=200)
     bob.turn_front_motor(-300, 300)
-    # Topsoil removed
+    # Top soil removed
 
-    bob.foreward(distance=-10, speed=200, then=Stop.HOLD)
-    bob.turn(degree=30, speed=100, then=Stop.HOLD)
-    bob.foreward(distance=-400, speed=450, then=Stop.HOLD)
-    bob.foreward(distance=400, speed=450, then=Stop.HOLD)
-    # Soil deposits cleared
-
-    bob.turn(degree=-30, speed=100, then=Stop.HOLD)
-    bob.foreward(distance=10, speed=500, then=Stop.HOLD)
-    bob.foreward(distance=-25, speed=1000, then=Stop.HOLD)
-    bob.turn(degree=-70, speed=100, then=Stop.HOLD)
-    bob.foreward(distance=-250, speed=200, then=Stop.HOLD)
-    bob.turn(degree=64, speed=60, then=Stop.BRAKE)
+    bob.arc(radius=50, angle=-100, speed=75, then=Stop.HOLD)
+    bob.arc(radius=-160, angle=-75, speed=75, then=Stop.BRAKE)
     bob.turn_back_motor_until_stalled(speed=450, duty_limit=40)
-    # Lifting arm down
-
-    bob.foreward(distance=-175, speed=200, then=Stop.BRAKE)
-
-    bob.turn_back_motor_dc(dc=-100, time=150)
+    bob.foreward(distance=-90, speed=100, then=Stop.COAST)
+    # In position for statue rebuild
+    
+    bob.turn_back_motor_dc(dc=-100, time=200)
     wait(200)
     bob.turn(degree=-30, speed=100, then=Stop.COAST)
     wait(200)
     bob.turn(degree=30, speed=100, then=Stop.COAST)
-    # Statue rebuild done
+    # Statue rebuilt
 
-    bob.foreward(distance=100, speed=100, then=Stop.COAST)
-    bob.turn_back_motor_until_stalled(speed=450, duty_limit=40)
-    bob.drivebase.arc(radius=-195, distance=500, then=Stop.COAST)
-    bob.foreward(distance=618, speed=500)
+    bob.arc(radius=-200, angle=85, speed=500, then=Stop.COAST)
+    bob.foreward(distance=600, speed=500)
     # Back home
