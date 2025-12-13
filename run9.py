0a01c5603f71ec4529b98394cc22bc1519e1cf02
RishtheFish001 <rishaan.krishnan@gmail.com>
2025-12-13 14:22:39 -0800

    Updated run9.py
    What's on sale completed on pybricks

--- run9.py
+++ run9.py
@@ -1,10 +1,11 @@
 from bob import Bob
 
 def execute(bob: Bob):
-    bob.foreward_and_front_motor(355,200,400,150)
+
+    bob.foreward_and_front_motor(335,200,325,200)
 
     bob.foreward(-270,300)
 
-    bob.foreward_and_front_motor(125,300,-325,200)
+    bob.foreward_and_front_motor(100,300,-325,200)
 
     bob.foreward(-300,200)
