d165c75361fd75cb943d2002064c91f248282bd9
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2025-12-17 17:25:51 -0800

    updated run 2 to cover multiple challenges and edited bob with hansikas turn

--- run2.py
+++ run2.py
@@ -4,34 +4,78 @@ from pybricks.hubs import PrimeHub
 from pybricks.pupdevices import Motor
 from pybricks.parameters import Port, Direction, Stop
 from pybricks.robotics import DriveBase
-from pybricks.tools import wait
+from pybricks.tools import wait, multitask
 
 from bob import Bob
 
 def execute(bob: Bob):
-    bob.foreward_and_front_motor(foreward_distance=525, foreward_speed=400, turn_degree=-110, turn_speed=300)
-    bob.foreward(-150, 300)
-    bob.foreward(200, 400)
-    # Soil deposit removed
+    bob.turn_front_motor(200,100)
+    # Ready to start
 
-    bob.turn_front_motor(80, 100)
-    bob.arc(radius=-460, angle=25, speed=200)
-    bob.turn_front_motor(-300, 300)
-    # Top soil removed
+    bob.foreward(800,200)
+    bob.turn_back_motor(-500,200)
+    # Back attachment ready for minecart
+    bob.foreward(150,100)
+    bob.turn_back_motor(200,100)
+    bob.turn_back_motor(-200,100)
+    # Minecart done
 
-    bob.arc(radius=50, angle=-100, speed=75, then=Stop.HOLD)
-    bob.arc(radius=-160, angle=-75, speed=75, then=Stop.BRAKE)
-    bob.turn_back_motor_until_stalled(speed=450, duty_limit=40)
-    bob.foreward(distance=-90, speed=100, then=Stop.COAST)
-    # In position for statue rebuild
+    bob.foreward(-160,200)
+    bob.turn_back_motor(400, 400)
+    bob.turn(-47,100)
+    bob.foreward(-100,100)
+    bob.turn_front_motor(280,100)
+    bob.foreward(85,100)
+    bob.foreward(55,100)
+    bob.foreward(-40,100)
+    bob.turn_front_motor(-350,100)
+    bob.turn(45,100)
+    bob.foreward(-300,400)
+    bob.foreward(-600,400)
+
+
+
+    # bob.foreward(800,200)
+    # bob.turn_back_motor(-500,200)
+    # bob.foreward(100,100)
+    # bob.turn_back_motor(200,200)
+    # bob.turn_back_motor(-200,200)
+    # bob.foreward(800,400)
+    # bob.turn_back_motor(-600,200)
+    # bob.foreward(100,200)
+    # bob.turn_back_motor(300,200)
+    # bob.turn_back_motor(-290,200)
+    # bob.foreward(-100,200)
+    # bob.turn_back_motor(400,200)
+    # bob.turn(-45,100)
+    # bob.foreward(-100,100)
+    # bob.turn_front_motor(350,100)
+    # bob.foreward(300,100)
+    # bob.turn_front_motor(-400,100)
     
-    bob.turn_back_motor_dc(dc=-100, time=200)
-    wait(200)
-    bob.turn(degree=-30, speed=100, then=Stop.COAST)
-    wait(200)
-    bob.turn(degree=30, speed=100, then=Stop.COAST)
-    # Statue rebuilt
+    # bob.foreward_and_front_motor(foreward_distance=525, foreward_speed=400, turn_degree=-110, turn_speed=300)
+    # bob.foreward(-150, 300)
+    # bob.foreward(200, 400)
+    # # Soil deposit removed
 
-    bob.arc(radius=-200, angle=85, speed=500, then=Stop.COAST)
-    bob.foreward(distance=600, speed=500)
-    # Back home
+    # bob.turn_front_motor(80, 100)
+    # bob.arc(radius=-460, angle=25, speed=200)
+    # bob.turn_front_motor(-300, 300)
+    # # Top soil removed
+
+    # bob.arc(radius=50, angle=-100, speed=75, then=Stop.HOLD)
+    # bob.arc(radius=-160, angle=-75, speed=75, then=Stop.BRAKE)
+    # bob.turn_back_motor_until_stalled(speed=450, duty_limit=40)
+    # bob.foreward(distance=-90, speed=100, then=Stop.COAST)
+    # # In position for statue rebuild
+    
+    # bob.turn_back_motor_dc(dc=-100, time=200)
+    # wait(200)
+    # bob.turn(degree=-30, speed=100, then=Stop.COAST)
+    # wait(200)
+    # bob.turn(degree=30, speed=100, then=Stop.COAST)
+    # # Statue rebuilt
+
+    # bob.arc(radius=-200, angle=85, speed=500, then=Stop.COAST)
+    # bob.foreward(distance=600, speed=500)
+    # # Back home
