cede4bb9276b2f60cb4cd67281aa28c5e75ede57
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2026-01-17 19:02:09 -0800

    statue rebuild, flag,  and angler atrifacts completed

--- run1.py
+++ run1.py
@@ -9,7 +9,35 @@ from pybricks.tools import wait
 from bob import Bob
 
 def execute(bob: Bob):
-    bob.foreward(450,200)
-    bob.foreward(-150,200)
-    bob.foreward(250,200)
-    bob.foreward(-600,500)
+    # bob.foreward(660,100)
+    # bob.foreward(-100,100)
+
+
+    bob.foreward_and_front_motor(480,250,-50,250, Stop.HOLD)
+    bob.turn_front_motor(50,250)
+    bob.foreward(-50,250)
+    bob.turn_front_motor(-300,250)
+    bob.foreward(270,250)
+    bob.turn_back_motor(100,250)
+    bob.foreward(-640,400)
+
+
+    # bob.foreward(480,100)
+    # bob.turn_front_motor(420,100)
+    # bob.foreward(-50,100)
+    # bob.turn_front_motor(-300,100)
+    # bob.foreward(270,100)
+    # bob.turn_back_motor(100,60)
+    # bob.foreward(-640,300)
+
+
+    # bob.foreward(80,100)
+    # bob.turn(100,100)
+    # bob.foreward(32,100)
+    # bob.turn(-15,60)
+    # bob.turn_front_motor(-2000,300)
+
+    # bob.foreward(450,200)
+    # bob.foreward(-150,200)
+    # bob.foreward(250,200)
+    # bob.foreward(-600,500)
--- run3.py
+++ run3.py
@@ -4,18 +4,24 @@ from pybricks.tools import wait
 from pybricks.parameters import Stop
 
 def execute(bob: Bob):
-    bob.foreward(distance=320, speed=300, then=Stop.BRAKE)
-    bob.arc(radius=550, angle=50, then=Stop.BRAKE)
-    bob.turn_front_motor_dc(dc=70, time=650)
-    wait(1000)
-    bob.turn_front_motor(degree=-180, speed=200, then=Stop.COAST)
-    # Minecart sent
+    bob.foreward(980,100)
+    bob.turn(88,100)
+    bob.foreward(50,100)
+    bob.turn(-12,60)
+    bob.turn_front_motor(-2000,300)
 
-    bob.turn_back_motor(degree=370, speed=400)
-    bob.arc(radius=-450, angle=-40, speed=200)
-    bob.turn_back_motor(degree=-120, speed=100)
-    # Brush picked up
+    # bob.foreward(distance=320, speed=300, then=Stop.BRAKE)
+    # bob.arc(radius=550, angle=50, then=Stop.BRAKE)
+    # bob.turn_front_motor_dc(dc=70, time=650)
+    # wait(1000)
+    # bob.turn_front_motor(degree=-180, speed=200, then=Stop.COAST)
+    # # Minecart sent
+
+    # bob.turn_back_motor(degree=370, speed=400)
+    # bob.arc(radius=-450, angle=-40, speed=200)
+    # bob.turn_back_motor(degree=-120, speed=100)
+    # # Brush picked up
     
-    bob.arc(radius=90, angle=90, then=Stop.BRAKE)
-    bob.foreward_and_front_motor(foreward_distance=450, foreward_speed=500, turn_degree=180, turn_speed=200)
-    # Back home
+    # bob.arc(radius=90, angle=90, then=Stop.BRAK)
+    # bob.foreward_and_front_motor(foreward_distance=450, foreward_speed=500, turn_degree=180, turn_speed=200)
+    # # Back home
