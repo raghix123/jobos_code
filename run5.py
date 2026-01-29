0c0f73e26464338620e4a93fa631f799dd339e87
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2026-01-28 18:04:18 -0800

    wrote code solves 2: tip the scales and whats on sale

--- run5.py
+++ run5.py
@@ -9,13 +9,30 @@ from pybricks.tools import wait
 from bob import Bob
 
 def execute(bob: Bob):
-    yield from bob.turn_front_motor(degree=-50, speed=100)
+    yield from bob.turn_front_motor(degree=-45, speed=100)
     # Ready
 
-    yield from bob.foreward(distance=285, speed=200)
-    yield from bob.turn(degree=-60, speed=75, then=Stop.BRAKE)
+    yield from bob.foreward(distance=250, speed=200)
+
+
+    yield from bob.turn(degree=-45, speed=75, then=Stop.BRAKE)
     # Marketwares solved
-    yield from bob.arc(radius= 1700,angle=15,speed=100)
+    
+    yield from bob.foreward(distance=160,speed=100)
+    yield from bob.turn(degree=26, speed=100)
+
+    yield from bob.turn_front_motor(degree=60, speed=100)
+    yield from bob.turn(degree=-45, speed=75, then=Stop.HOLD)
+    wait(500)
+    yield from bob.turn(degree=15, speed=75, then=Stop.HOLD)
+    # Tip the scales B done
+
+    yield from bob.foreward(distance=400, speed=800)
+    yield from bob.turn(-20,60)
+    yield from bob.foreward(650,800)
+    # # Back home
+    return
+    yield from bob.arc(radius= 1400,angle=15,speed=100)
     # yield from bob.turn(degree=45, speed=75, then=Stop.BRAKE)
     # yield from bob.foreward(distance=-100,speed=100)
     # yield from bob.turn(degree=-45, speed=75, then=Stop.BRAKE)
@@ -27,7 +44,7 @@ def execute(bob: Bob):
     yield from bob.foreward(distance=-210, speed=200)
     yield from bob.turn(degree=32, speed=75, then=Stop.BRAKE)
     yield from bob.turn_front_motor(degree=60, speed=100)
-    yield from bob.turn(degree=-38, speed=75, then=Stop.HOLD)
+    yield from bob.turn(degree=-45, speed=75, then=Stop.HOLD)
     wait(500)
     # Tip the scales B done
 
