30820140f1d69b067d1fd845758eb75992712a14
Rishaan Krishnan <rishaan.krishnan@gmail.com>
2026-02-18 19:00:53 -0800

    run2, run1 fixed

--- run9.py
+++ run9.py
@@ -6,7 +6,8 @@ from bob import Bob
 def execute(bob: Bob):
     yield from bob.foreward(distance=420, speed=400)
     yield from bob.turn_front_motor_dc(dc=40, time=500)
-    yield from bob.foreward(distance=20, speed=100)
+    yield from bob.foreward(distance=40, speed=100)
     yield from bob.turn_front_motor(degree=-90, speed=100)
-    yield from bob.foreward_and_front_motor(foreward_distance=80, foreward_speed=100, turn_degree=-50, turn_speed=100)
+    # yield from bob.foreward_and_front_motor(foreward_distance=150, foreward_speed=100, turn_degree=-80, turn_speed=500)
+    yield from bob.turn_front_motor(-200,500)
     yield from bob.foreward(distance=-50, speed=100)
