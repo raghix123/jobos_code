a917ac4fb9a7c6c16c79d875f5ff86976c1435c6
Kasturi Bodicherla <kasturi.bodicherla@gmail.com>
2026-02-22 11:07:20 -0800

    run10 more or locked - turn to exit instead of lift

--- run10.py
+++ run10.py
@@ -3,7 +3,7 @@ from bob import Bob
 from pybricks.parameters import Stop
 
 def execute(bob: Bob):
-    yield from bob.foreward(25,60)
+    yield from bob.foreward(20,60)
 
     yield from bob.turn_front_motor(240,150)
 
@@ -15,15 +15,17 @@ def execute(bob: Bob):
 
     yield from bob.foreward(280,100)
     
-    yield from bob.run_front_motor_until_stalled(250, then=Stop.HOLD, duty_limit=30)
+    yield from bob.run_front_motor_until_stalled(400, then=Stop.HOLD, duty_limit=30)
     
     wait(100)
     
-    yield from bob.foreward_and_front_motor(-80,180,-350,150)
+    # yield from bob.foreward_and_front_motor(-80,180,-350,150)
 
-    yield from bob.foreward(-350, 300)
+    yield from bob.foreward(-400, 300)
 
-    yield from bob.foreward(50,200)
+    yield from bob.foreward(20,100)
+
+    yield from bob.turn(45,200)
 
     yield from bob.turn_front_motor(-350,100)
 
